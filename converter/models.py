ec6cf21218df1bbe4246ee7bd7bfb341a9862c5d
sara12359 <sarasalahmohamed97@gmail.com>
2026-03-22 14:49:36 +0200

    ex

--- converter/models.py
+++ converter/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+class Currency(models.Model):
+    code = models.CharField(max_length=3, unique=True)
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.code} - {self.name}"
+
+    class Meta:
+        verbose_name_plural = "Currencies"
+
+class ExchangeRate(models.Model):
+    base_currency = models.CharField(max_length=3, default='USD')
+    target_currency = models.CharField(max_length=3)
+    rate = models.DecimalField(max_digits=20, decimal_places=10)
+    last_updated_api = models.DateTimeField(null=True, blank=True)
+    last_updated_local = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        unique_together = ('base_currency', 'target_currency')
+
+    def __str__(self):
+        return f"1 {self.base_currency} = {self.rate} {self.target_currency}"
